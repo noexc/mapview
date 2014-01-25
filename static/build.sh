f2ea73e3ebd5af1b3a95afab896645be93c44563
Ricky Elrod <ricky@elrod.me>
2014-01-25 04:48:05 -0500

    Use external extern for google maps extern

--- static/build.sh
+++ static/build.sh
@@ -6,6 +6,6 @@ pushd "$cwd/.."
 .cabal-sandbox/bin/fay --include --include=../.cabal-sandbox --package fay-text,fay-dom "$cwd/mapview.hs"
 echo "Using Google's Closure Compiler with 'advanced optimizations'"
 echo "This might be slow on some computers..."
-closure-compiler --externs "$cwd/google_maps_api_v3_11.js" --compilation_level ADVANCED_OPTIMIZATIONS "$cwd/mapview.js" > "$cwd/mapview.min.js" 2>/dev/null
+closure-compiler --externs /usr/share/closure-compiler/externs/contrib/maps/google_maps_api_v3_15.js --compilation_level ADVANCED_OPTIMIZATIONS "$cwd/mapview.js" > "$cwd/mapview.min.js" 2>/dev/null
 rm "$cwd/mapview.js"
 popd
