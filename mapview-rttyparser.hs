d8c080910718ddd32b3fbfd45ab7468e14006391
Ricky Elrod <ricky@elrod.me>
2014-06-15 04:12:03 -0400

    Unhardcode paths in rttyparser & rename it
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- mapview-rttyparser.hs
+++ mapview-rttyparser.hs
@@ -9,6 +9,8 @@ import Control.Monad.IO.Class
 import Control.Lens
 import qualified Data.Aeson as A
 import Data.Attoparsec.Text
+import qualified Data.Configurator as Cfg
+import Data.List (dropWhileEnd)
 import Data.Maybe (fromMaybe)
 import Data.Thyme.Clock
 import Data.Thyme.Format
@@ -16,11 +18,12 @@ import Data.Thyme.Format.Aeson ()
 import qualified Data.ByteString.Lazy.Char8 as C8L
 import qualified Data.Text as T
 import GHC.IO.Handle
+import Options.Applicative hiding (Parser)
+import qualified Options.Applicative (Parser)
 import Shelly hiding (time)
-import System.Directory (createDirectoryIfMissing)
+import System.Directory (createDirectoryIfMissing, doesFileExist)
 import qualified System.IO.Strict as S
 import System.Locale
-import System.Posix.Files
 
 type Latitude  = Double
 type Longitude = Double
@@ -68,31 +71,72 @@ instance A.ToJSON RTTYLine where
     , "time"        A..= t
     ]
 
+data CLIOptions =
+  CLIOptions { configFile :: String } deriving (Show)
+
+data RTTYPaths =
+  RTTYPaths { historyPath :: String
+            , rawLogPath :: String
+            , workingPath :: String
+            }
+
+parseOptions :: Options.Applicative.Parser CLIOptions
+parseOptions =
+  CLIOptions <$> strOption (long "conf"
+                            <> short 'c'
+                            <> metavar "CONFIG_FILE"
+                            <> value "mapview.conf")
+
 main :: IO ()
-main = do
-  createDirectoryIfMissing True "/tmp/w8upd"
-  createDirectoryIfMissing True "/var/tmp/w8upd"
-  touchFile "/var/tmp/w8upd/coordinates-log.json"
-  touchFile "/var/tmp/w8upd/rttylog"
-  touchFile "/tmp/w8upd/rtty-coordinates.json"
-  readRTTY
+main = execParser opts >>= runMain
+  where
+    opts = info (helper <*> parseOptions)
+      ( fullDesc
+     <> progDesc "Read RTTY telemetry from minimodem"
+     <> header "mapview-rttyparser - Read RTTY telemetry from minimodem" )
 
-readRTTY :: IO ()
-readRTTY = shelly $ runHandle "minimodem" ["-r", "-q", "rtty", "-S", "700", "-M", "870"] writeJson
+runMain :: CLIOptions -> IO ()
+runMain (CLIOptions configFile') = do
+  config <- Cfg.load [Cfg.Required $ configFile']
+  historyPath' <- (Cfg.require config "telemetry.coordinates-history") :: IO String
+  createDirectoryIfMissing True (baseDir historyPath')
 
-recordCoordinates :: Coordinates -> IO ()
-recordCoordinates latest = do
-  old <- S.readFile "/var/tmp/w8upd/coordinates-log.json"
+  rawLogPath' <- (Cfg.require config "telemetry.raw-log") :: IO String
+  createDirectoryIfMissing True (baseDir rawLogPath')
+
+  workingPath' <- (Cfg.require config "telemetry.working-coordinates") :: IO String
+  createDirectoryIfMissing True (baseDir workingPath')
+
+  let paths = RTTYPaths historyPath' rawLogPath' workingPath'
+
+  mapM_ createFileIfMissing [historyPath', rawLogPath', workingPath']
+
+  readRTTY paths
+  where
+    baseDir = init . dropWhileEnd (/= '/')
+
+    createFileIfMissing p = do
+      doesExist <- doesFileExist p
+      if doesExist
+        then return ()
+        else writeFile p ""
+
+readRTTY :: RTTYPaths -> IO ()
+readRTTY p = shelly $ runHandle "minimodem" ["-r", "-q", "rtty", "-S", "700", "-M", "870"] (writeJson p)
+
+recordCoordinates :: RTTYPaths -> Coordinates -> IO ()
+recordCoordinates p latest = do
+  old <- S.readFile (historyPath p)
   let cList = A.decode (C8L.pack old) :: Maybe [Coordinates]
-  writeFile "/var/tmp/w8upd/coordinates-log.json" (C8L.unpack $ A.encode (latest : fromMaybe [] cList))
+  writeFile (historyPath p) (C8L.unpack $ A.encode (latest : fromMaybe [] cList))
 
-writeJson :: Handle -> Sh ()
-writeJson h = do
+writeJson :: RTTYPaths -> Handle -> Sh ()
+writeJson p h = do
   liftIO $ hSetBuffering h NoBuffering
   line <- liftIO $ hGetLine h
-  when (not (null line) && (line /= "RRRRR")) $ do
+  when (not (null line)) $ do
       liftIO $ putStrLn $ "RECEIVED LINE: " ++ line
-      liftIO $ appendFile "/var/tmp/w8upd/rttylog" (line ++ "\n")
+      liftIO $ appendFile (rawLogPath p) (line ++ "\n")
       case parseOnly parseLine (T.pack line) of
         Left err -> liftIO $ putStrLn $ "ERROR: " ++ err
         Right rttyLine'' -> do
@@ -100,9 +144,9 @@ writeJson h = do
           rttyLine' <- liftIO rttyLine''
           let rttyLine = time . _utctDay .~ currentDay ^. _utctDay $ rttyLine'
           liftIO $ putStrLn $ "...which parsed into: " ++ show rttyLine
-          liftIO $ writeFile "/tmp/w8upd/rtty-coordinates.json" (C8L.unpack $ A.encode rttyLine)
-          liftIO $ recordCoordinates (rttyLine ^. coordinates)
-  writeJson h
+          liftIO $ writeFile (workingPath p) (C8L.unpack $ A.encode rttyLine)
+          liftIO $ recordCoordinates p (rttyLine ^. coordinates)
+  writeJson p h
 
 parseLine :: Parser (IO RTTYLine)
 parseLine = do
