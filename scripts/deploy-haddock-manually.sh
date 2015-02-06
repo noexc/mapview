782e6ae593f7b7bfddbffa9473578ad9aed7c217
Ricky Elrod <ricky@elrod.me>
2015-02-06 04:18:53 -0500

    play with asciidoctor for docs
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- scripts/deploy-haddock-manually.sh
+++ scripts/deploy-haddock-manually.sh
@@ -7,9 +7,16 @@ git clone git@github.com:noexc/mapview.git "$f/mapview.git"
 cabal haddock
 pushd "$f/mapview.git"
   git checkout gh-pages && git rm -rf *
+  mkdir haddock
 popd
-mv dist/doc/html/mapview/* "$f/mapview.git/"
+mv dist/doc/html/mapview/* "$f/mapview.git/haddock"
 pushd "$f/mapview.git"
+  git checkout master -- docs
+  pushd docs
+    asciidoctor *.adoc
+    ln -s Introduction.html index.html
+    rm *.adoc
+  popd
   git add -A
   git commit -m "Manual docs deploy."
   git push origin gh-pages
