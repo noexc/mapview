75f8292a8fce6e38c4bcfb9d3a560f14f9e0f263
Ricky Elrod <ricky@elrod.me>
2015-11-12 15:05:51 -0500

    new Execute module
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ src/KD8ZRC/Mapview/Execute.hs
@@ -0,0 +1,24 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module : KD8ZRC.Mapview.Execute
+-- Copyright : (C) 2015 Ricky Elrod
+-- License : MIT (see LICENSE file)
+-- Maintainer : Ricky Elrod <ricky@elrod.me>
+-- Stability : experimental
+-- Portability : ghc
+--
+-- Provides a function that kicks off the backend (TODO: better description once
+-- we have a better idea of how this will actually work).
+----------------------------------------------------------------------------
+module KD8ZRC.Mapview.Execute where
+
+import Control.Lens
+import Control.Monad.Trans.Reader
+import KD8ZRC.Mapview.Types
+
+-- | Takes a 'MapviewConfig' and uses its '_mvDownlinkSpawn' callback to start
+-- our backend process.
+--
+-- TODO: Threading stuff -- where, when, how?
+mapview :: MapviewConfig t -> IO ()
+mapview config = runReaderT (config ^. mvDownlinkSpawn) config
