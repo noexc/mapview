b9e54cf59170fcd208f6e7e587c93404bc280518
Ricky Elrod <ricky@elrod.me>
2016-01-04 08:38:17 -0500

    Add a callback to update a Chan on packet rx.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ src/KD8ZRC/Mapview/Utility/Concurrent.hs
@@ -0,0 +1,26 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module : KD8ZRC.Mapview.Utility.Concurrent
+-- Copyright : (C) 2015 Ricky Elrod
+-- License : MIT (see LICENSE file)
+-- Maintainer : Ricky Elrod <ricky@elrod.me>
+-- Stability : experimental
+-- Portability : ghc
+--
+-- Provides callbacks to aid in concurrent applications (e.g., updating a 'Chan'
+-- at various stages).
+----------------------------------------------------------------------------
+module KD8ZRC.Mapview.Utility.Concurrent where
+
+import qualified Control.Concurrent.Chan as Chan
+import Control.Monad.IO.Class
+import qualified Data.Text as T
+import KD8ZRC.Mapview.Types
+
+--------------------------------------------------------------------------------
+-- Raw Packet Callbacks
+--------------------------------------------------------------------------------
+
+writeChanRaw :: Chan.Chan T.Text -> PacketLineCallback t
+writeChanRaw ch =
+  PacketLineCallback (\raw -> liftIO $ Chan.writeChan ch (T.pack raw))
