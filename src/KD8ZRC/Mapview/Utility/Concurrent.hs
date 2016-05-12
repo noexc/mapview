a660e334c52efad8417cda3d40238b9bd49adf37
Ricky Elrod <ricky@elrod.me>
2016-05-12 02:58:01 -0400

    Move writeChanJsonPkt to Concurrent from -noexc
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/KD8ZRC/Mapview/Utility/Concurrent.hs
+++ src/KD8ZRC/Mapview/Utility/Concurrent.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module : KD8ZRC.Mapview.Utility.Concurrent
--- Copyright : (C) 2015 Ricky Elrod
+-- Copyright : (C) 2016 Ricky Elrod
 -- License : MIT (see LICENSE file)
 -- Maintainer : Ricky Elrod <ricky@elrod.me>
 -- Stability : experimental
@@ -14,7 +14,9 @@ module KD8ZRC.Mapview.Utility.Concurrent where
 
 import qualified Control.Concurrent.Chan as Chan
 import Control.Monad.IO.Class
+import Data.Aeson (ToJSON, encode)
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BSL
 import KD8ZRC.Mapview.Types
 
 --------------------------------------------------------------------------------
@@ -24,3 +26,10 @@ import KD8ZRC.Mapview.Types
 writeChanRaw :: Chan.Chan BS.ByteString -> PacketLineCallback t
 writeChanRaw ch =
   PacketLineCallback (\raw -> liftIO $ Chan.writeChan ch raw)
+
+-- | If we know that we can JSON-encode a packet, then we can use this fact to
+-- send an encoded version of the packet to the web-socket automagically.
+writeChanJsonPkt :: ToJSON t => Chan.Chan BS.ByteString -> ParsedPacketCallback t
+writeChanJsonPkt ch =
+  ParseSuccessCallback (
+    \pkt -> liftIO $ Chan.writeChan ch (BSL.toStrict . encode $ pkt))
